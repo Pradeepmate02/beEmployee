bd59ddaa0086635eafe54ab28dd403aca9b8ae36
Pradeepmate02 <155256317+Pradeepmate02@users.noreply.github.com>
2026-01-09 14:16:00 +0530

    Update webHooks.js

--- server/controllers/webHooks.js
+++ server/controllers/webHooks.js
@@ -57,7 +57,7 @@ export const clerkwebhooks = async (req, res) => {
                 break;
         }
     } catch (error) {
-        console.log(error.message);
-        res.json({success: false, message : 'webHooks error'})
+       
+        res.json({success: false, message : error.message})
     }
 }
